af89cb27ae4b4cf0a239bde70519e9b490599516
Salem <musblum@gmail.com>
2024-09-12 14:13:37 -0400

    Pink color to event

--- 01.StockResearch/stock-research/src/main/java/edu/citytech/cst3613/SearchStockController.java
+++ 01.StockResearch/stock-research/src/main/java/edu/citytech/cst3613/SearchStockController.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.FlowPane;
@@ -24,6 +25,9 @@ public class SearchStockController implements Initializable {
 
     @FXML
     private Label lblTitle;
+
+    @FXML
+    private FlowPane fpMonths;
 
     @FXML
     private ToggleGroup tgMarketCap;
@@ -54,6 +58,28 @@ public class SearchStockController implements Initializable {
         int multipleOf = Integer.parseInt(sUserData);
         System.out.println(event);
         count(0,multipleOf);
+    }
+
+    
+    @FXML
+    void monthSelections(ActionEvent event) {
+        var checkbox  = (CheckBox)event.getSource();
+
+        System.out.println(event);
+        System.out.println(checkbox.getUserData());
+        
+        int monthNo = Integer.parseInt(checkbox.getUserData().toString());
+
+        for ( Node node : stocks) {
+            Label label = (Label)node;
+            int  number = Integer.parseInt(label.getText().trim());
+
+            label.getStyleClass().remove("month-color");
+            if(number % monthNo == 0)
+                label.getStyleClass().add("month-color");
+                
+            
+        } 
     }
 
 }
